988074de9b3dfa41c7cc162d68ce10400db69156
Administrator <Administrator@6QAKH8KYZWX4N4Z>
2017-04-21 09:11:49 +0800

    添加项目文件。

---
+++ VideoWebDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webdiyer.MvcPagerDemo.Models;
+
+
+namespace VideoWebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult VideoList(int id = 1)
+        {
+            return View(DemoData.AllArticles.OrderByDescending(a => a.PubDate).ToPagedList(id, 5));
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ VideoWebDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoWebDemo.Startup))]
+namespace VideoWebDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
